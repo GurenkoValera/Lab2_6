b2835970efe503227f3bdd8db21c7201b21ad848
valer <valer@DESKTOP-QD94NVR>
2024-10-28 19:36:19 +0300

    Добавьте файлы проекта.

---
+++ Lab2_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число от 1 до 100:");
+        int N;
+
+        // Проверяем, является ли ввод корректным
+        while (!int.TryParse(Console.ReadLine(), out N) || N < 1 || N > 100)
+        {
+            Console.WriteLine("Пожалуйста, введите корректное число от 1 до 100:");
+        }
+
+        // Определяем правильное окончание
+        string ending;
+        if (N % 10 == 1 && N % 100 != 11)
+        {
+            ending = "год";
+        }
+        else if ((N % 10 >= 2 && N % 10 <= 4) && (N % 100 < 10 || N % 100 >= 20))
+        {
+            ending = "года";
+        }
+        else
+        {
+            ending = "лет";
+        }
+
+        // Выводим результат
+        Console.WriteLine($"{N} {ending}");
+    }
+}
